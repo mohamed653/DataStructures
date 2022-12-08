b6196944d72ca33f7decd23f0f71eed0ae55fb48
Mohamed Hamed <mohamedhamed3343@gmail.com>
2022-12-09 01:52:49 +0200

    Implementing Stack Using Array

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,168 +6,87 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Node<double> nOne = new Node<double>(1);
-            Node<double> nTwo = new Node<double>(2);
-            Node<double> nFive = new Node<double>(5);
-            Node<double> nSeven = new Node<double>(7);
-            Node<double> nEight = new Node<double>(8);
-            Node<double> nFour = new Node<double>(4);
-            //one.Next= two;
-            //two.Next= five;
-            //Console.WriteLine(one.Next.Data);
-
-            //Contructing the linked list
-            Linked_List<double> linked_List = new Linked_List<double>();
-            linked_List.AddEndNode(nOne);
-            linked_List.AddEndNode(nTwo);
-            linked_List.AddEndNode(nFive);
-            linked_List.AddEndNode(nSeven);
-            linked_List.AddEndNode(nEight);
-            linked_List.AddEndNode(nFour);
-
-            //Traverseing the linked list
-            Console.WriteLine("Traverseing the linked list");
-            linked_List.Traverse();
-
-            // Searching the linked list by node
-            Console.WriteLine("Searching the linked list by node");
-            int nodePosition = linked_List.GetPostion(nFive);
-            Console.WriteLine($"{nodePosition}");
-
-            // Searching the linked list by postion
-            Console.WriteLine("Searching the linked list by postion");
-            Node<double> mynode = linked_List.SearchNode(3);
-            Console.WriteLine($"mynode data: {mynode.Data}");
-
-            // Removing a node in linked list
-            Console.WriteLine("Removing a node in linked list");
-            linked_List.RemoveNode(nTwo);
-            linked_List.Traverse();
-
-            // Clearing LinkedList
-            Console.WriteLine("Clearing LinkedList");
-            linked_List.ClearList();
-            linked_List.Traverse();
+            StackArray<string> stackArray = new StackArray<string>(5);
+            stackArray.Push("a");
+            stackArray.Push("b");
+            stackArray.Push("c");
+            stackArray.Push("d");
+            stackArray.Push("e");
+            Console.WriteLine($"{stackArray.Pop()} is poped from stack");
+            stackArray.Display();
+            Console.WriteLine("POP DISPLAY:");
+            stackArray.PopDisplay(stackArray);
 
         }
     }
-    class Node<T>
+
+    // Implementing Stack Using Array
+    class StackArray<T>
     {
-        T data;
-        Node<T> next;
-        public Node(T data)
+        T[] array;
+        private int count;
+        private int top;
+        public int Count
         {
-            this.data = data;
+            get { return count; }
+            set { count = value; }
+        }
+        // Constructor
+        public StackArray(int size)
+        {
+            array = new T[size];
+            count = 0;
+            top = -1;
         }
 
-        public T Data
+        public void Push(T val)
         {
-            get { return data; }
-            set { data = value; }
+            if (!IsFull())
+            {
+                array[++top] = val;
+                count++;
+            }
+            else Console.WriteLine("Stack OverFlow!");
         }
-        public Node<T> Next
+        public T Pop()
         {
-            get { return next; }
-            set { next = value; }
+            if (!IsEmpty())
+            {
+                count--;
+                return array[top--];
+            }
+            else throw new Exception("Stack UnderFlow!");
         }
-    }
-    class Linked_List<T>
-    {
-        Node<T> head;
-        Node<T> tail;
-        public int Size; 
-
-        public Linked_List()
+        public bool IsEmpty() 
         {
-            head = tail = null;
-            Size = 0;
+            return (count==0)?true:false;
         }
-        public void AddEndNode(Node<T> node)
+        public bool IsFull()
         {
-            if (head ==null)
-            {
-                head =tail= node;
-            }
-            else
-            {
-                tail.Next = node;
-                tail = node;
-            }
-            Size++;
+            return (count == array.Length) ? true : false;
         }
-        public void Traverse()
+        public void Display()
         {
-            Node<T> current = head;
-            while (current!=null)
+            for (int i = top; i >= 0; i--)
             {
-                Console.Write($"{current.Data} => ");
-                current = current.Next;
+                Console.WriteLine($"| {array[i]} |");
             }
-            Console.WriteLine("null");
+            Console.WriteLine("=====");
         }
-        public int GetPostion(Node<T> node)
+        public void PopDisplay(StackArray<T> tempStack)
         {
-            int pos=1;
-            Node<T> current = head;
-            while (current!=null)
+            while (tempStack.Count!=0)
             {
-                // can't use == |  so i used Equals()   it can work with any obj 
-                if (current.Data.Equals(node.Data))
-                {
-                    return pos;
-                }
-                current = current.Next;
-                pos++;
-            }
-            return -1;
+                Console.WriteLine(Pop());
+            }         
         }
-        public Node<T> SearchNode(int pos)
+        public T Peak()
         {
-            Node<T> fNode=head;
-            for (int i = 1; i < pos; i++)
+            if (!IsEmpty())
             {
-                fNode = fNode.Next;
+                return array[top];
             }
-            if (fNode!=null)
-            {
-                return fNode;
-            }
-            throw new Exception("No Node Found!");
-        }
-
-        // you have three cases:
-        //the node we want to remove is => 
-        // 1- theHeadNode
-        // 2- theTailNode
-        // 3-  a middle node "which is not a head or a tail node"
-        public void RemoveNode(Node<T> rNode)
-        {
-            int rPos = GetPostion(rNode);
-            // finding the previous node for case 2&3
-            Node<T> prevNode = SearchNode(rPos - 1);
-            if (rPos==1)
-            {
-                // this means that rNode is the headNode
-                head = head.Next;
-                // i used rNode as a temp
-                rNode.Next = null;
-            }
-            else if (rPos==Size)
-            {
-                // this means that rNode is the tailNode
-                tail = prevNode;
-                prevNode.Next = null;
-            }
-            else
-            {
-                prevNode.Next = rNode.Next;
-                rNode.Next = null;
-            }
-        }
-
-        public void ClearList()
-        {
-            head = tail=null;
+            else throw new Exception("Stack UnderFlow!");
         }
     }
 }
