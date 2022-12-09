29a4403e6a7e317616d6a47534aaabba9019aca2
Mohamed Hamed <mohamedhamed3343@gmail.com>
2022-12-09 06:19:18 +0200

    Binary Tree

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,22 +8,74 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            BNode<int> rootNode = new BNode<int>(5);
-            BNode<int> lnode = new BNode<int>(4);
-            rootNode.Left = lnode;
-            BNode<int> rnode = new BNode<int>(6);
-            rootNode.Right = rnode;
-            Console.WriteLine(rootNode.Left.data+" "+ rootNode.Right.data);
+            BTree bTree = new BTree();
+            bTree.Add(8);
+            bTree.Add(3);
+            bTree.Add(10);
+            bTree.Add(5);
+
         }
     }
-    class BNode<T>
+    class BNode
     {
-        public T data;
-        public BNode<T> Right;
-        public BNode<T> Left;
-        public BNode(T data)
+        public int data;
+        public BNode Right;
+        public BNode Left;
+        public BNode(int data)
         {
             this.data = data;
         }
+    }
+    /// <summary>
+    /// Binary Tree :have at most 2 childern (left child node, right child node)
+    /// </summary>
+    class BTree
+    {
+        public BNode Root;
+        public int count = 0;
+        public void Add(int value)
+        {
+            BNode bNode = new BNode(value);
+            if (Root == null)
+            {
+                Root = bNode;
+            }
+            else
+            {
+
+                AddTo(Root, value);
+            }
+            count++;
+        }
+        public void AddTo(BNode node, int value)
+        {
+            //declaring new node that wants to be added
+            BNode Nnode = new BNode(value);
+            if (value < node.data)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = Nnode;
+                }
+                else
+                {
+                    // *** RECURSION ***
+                    AddTo(node.Left, value);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = Nnode;
+                }
+                else
+                {
+                    // *** RECURSION ***
+                    AddTo(node.Right, value);
+                }
+            }
+        }
+
     }
 }
