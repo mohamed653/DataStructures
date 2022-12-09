8804ca6982d2c88ac82ebafa97a4c363c378f997
Mohamed Hamed <mohamedhamed3343@gmail.com>
2022-12-09 09:07:03 +0200

    Constructing Graphs

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,105 +8,28 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            BTree bTree = new BTree();
-            bTree.Add(8);
-            bTree.Add(3);
-            bTree.Add(10);
-            bTree.Add(5);
-            bTree.PreOrder(bTree.Root);
-            Console.WriteLine();
-            bTree.InOrder(bTree.Root);
-            Console.WriteLine();
-            bTree.PostOrder(bTree.Root);
-        }
-    }
-    class BNode
-    {
-        public int data;
-        public BNode Right;
-        public BNode Left;
-        public BNode(int data)
-        {
-            this.data = data;
-        }
+           
+        } 
     }
     /// <summary>
-    /// Binary Tree :have at most 2 childern (left child node, right child node)
+    /// Creating a graph Using Adjacency Lists
     /// </summary>
-    class BTree
+    class Graph
     {
-        public BNode Root;
-        public int count = 0;
-        public void Add(int value)
+        // total number of vertices => {n}
+        private int vertex;
+        // adjacency list which is {n} number of lists, each list contains one array
+        List<int>[] adjacency;
+        public Graph(int vertex)
         {
-            BNode bNode = new BNode(value);
-            if (Root == null)
+            adjacency = new List<int>[vertex];
+            for (int i = 0; i < adjacency.Length; i++)
             {
-                Root = bNode;
+                // now we created {n} lists from adjacency[0]=>adjacency[n-1]
+                adjacency[i] = new List<int>();
             }
-            else
-            {
+            this.vertex = vertex;
+        }
 
-                AddTo(Root, value);
-            }
-            count++;
-        }
-        public void AddTo(BNode node, int value)
-        {
-            //declaring new node that wants to be added
-            BNode Nnode = new BNode(value);
-            if (value < node.data)
-            {
-                if (node.Left == null)
-                {
-                    node.Left = Nnode;
-                }
-                else
-                {
-                    // *** RECURSION ***
-                    AddTo(node.Left, value);
-                }
-            }
-            else
-            {
-                if (node.Right == null)
-                {
-                    node.Right = Nnode;
-                }
-                else
-                {
-                    // *** RECURSION ***
-                    AddTo(node.Right, value);
-                }
-            }
-        }
-        public void PreOrder(BNode bNode)
-        {
-            if (bNode!=null)
-            {
-                Console.WriteLine(" "+bNode.data+" ");
-                PreOrder(bNode.Left);
-                PreOrder(bNode.Right);
-            }
-        }
-        public void InOrder(BNode bNode)
-        {
-            if (bNode != null)
-            {
-                InOrder(bNode.Left);
-                Console.WriteLine(" " + bNode.data + " ");
-                InOrder(bNode.Right);
-            }
-        }
-        public void PostOrder(BNode bNode)
-        {
-            if (bNode != null)
-            {
-                Console.WriteLine(" " + bNode.data + " ");
-                PostOrder(bNode.Right);
-                PostOrder(bNode.Left);
-
-            }
-        }
     }
 }
