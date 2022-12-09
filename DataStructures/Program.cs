007bb43dfaff0fe408004788919374740a581d65
Mohamed Hamed <mohamedhamed3343@gmail.com>
2022-12-09 04:04:02 +0200

    hashtable

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace DataStructures
 {
@@ -6,7 +7,27 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            
+
+            // 1. create a hashtable
+            Hashtable myTable = new Hashtable();
+
+            // 2. add items to hashtable
+            myTable.Add("Name", "Ginny");
+            myTable.Add("RollNumber", 12);
+            myTable.Add("Address", "Miami");
+            myTable.Add("key", "value");
+
+            // 3. remove element
+            myTable.Remove("key");
+
+            // 4. Access Hashtable
+            var i = myTable["Address"];
+
+            // 5. iterate/ traverse through hashtables
+            foreach (DictionaryEntry item in myTable)
+            {
+                Console.WriteLine(item.Key);
+            }
 
         }
     }
